9ebb2ccbfe5d9b96c32b83f199b3bcb748c73f7c
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:59:50 +0530

    Add files via upload

---
+++ I_O.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:38:17 2020
+
+@author: MOHANA D
+"""
+
+#Write a program to perform input/output of aall basic data types
+x=3 #x is int
+y=4.8 #y is float
+z="dasari" #z is string
+a=7j #a is complex
+b=["tv","phone","laptop"] #b is list
+c=("tv","phone","laptop") #c is tuple
+d={"tv","phone","laptop"} #d is set
+e=range(5) #e is range
+f=bool(2) #f is boolen
+print(x,y,z,a,b,c,d,e,f)
+print(type(x),type(y),type(z),type(a),type(b),type(c),type(d),type(e),type(f))
