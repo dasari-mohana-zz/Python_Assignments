6b33a265254bdae149a755736e32f4e18de22be4
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:56:25 +0530

    Add files via upload

---
+++ 3while.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 17:12:48 2020
+
+@author: MOHANA D
+"""
+
+#Print all 3 multiples from 1 to 100 using for while loop.
+
+i=1
+while i<100:
+    i=i+1
+    if i%3==0:
+      print(i)
+     
