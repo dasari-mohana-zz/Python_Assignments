5a43fcfd0b1594539f1071ce1036a80025604776
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:01:59 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:29:42 2020
+
+@author: MOHANA D
+"""
+
+#write a program to find power of any number
+x= float(input("enter base number:")) #base number
+y=float(input("enter power:")) #power number
+z=x**y
+print(z)
