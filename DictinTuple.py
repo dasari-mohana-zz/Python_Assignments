a6b89b944f19c9b2b304b8a57170841d7b0938d7
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:55:06 +0530

    Add files via upload

---
+++ DictinTuple.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 16:01:24 2020
+
+@author: MOHANA D
+"""
+
+	
+#Create a dictionary with following tuples (1,2), (2,3), (3,4).
+a=(1,2), (2,3), (3,4)
+dict1=dict(list(a)) #converted into list
+#dict1=dict(a)
+dict1
