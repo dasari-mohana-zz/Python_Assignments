0f444b38962687356322bd5b07eb138abea87306
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:58:23 +0530

    Add files via upload

---
+++ DaysYears.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:30:37 2020
+
+@author: MOHANA D
+"""
+
+#write a program to convert days into years, weeks and days
+d=float(input("enter no.of days:")) #days
+y=d/365 #y=years
+w=d/7 #w=weeks
+print(y)
+print("years=",y,"weeks=",w,"days=",d)
