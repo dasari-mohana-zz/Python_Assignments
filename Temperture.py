c7f820646f044441a27a8c59f0bb77299f08875a
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:04:18 +0530

    Add files via upload

---
+++ Temperture.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:32:11 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter temperature in celcius and convert into fahrenheit
+c=float(input("enter temperature in celcius:")) #c is celcius
+f=(c*1.8)+32 #f is farenheit
+print(f)
