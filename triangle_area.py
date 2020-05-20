3f1814c554d81108d9c83c5b0eaaf991613c6f75
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:04:48 +0530

    Add files via upload

---
+++ triangle_area.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:21:08 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter base and height of atraingle and find its area
+h=float(input("enter height value:"))
+b=float(input("enter base value:"))
+a=(b*h)/2
+print("area:",a)
