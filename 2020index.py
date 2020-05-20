db30bcc95cc5c2ea14061dc57fd3f093b6485fc4
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:56:48 +0530

    Add files via upload

---
+++ 2020index.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 15:55:40 2020
+
+@author: MOHANA D
+"""
+
+#Create a list with 5 elements, then insert 2020 at index 2.
+list1=[1,2,3,4,5]
+list1[2]=2020
+print(list1)
