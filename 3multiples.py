ab7e66fe0e38f8caec647f2638e6b45f46925adb
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:55:56 +0530

    Add files via upload

---
+++ 3multiples.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 15:44:34 2020
+
+@author: MOHANA D
+"""
+
+#Print all 3 multiples from 1 to 100 using for loop.
+for i in range(1,100):
+    if i%3==0:
+        print(i)
