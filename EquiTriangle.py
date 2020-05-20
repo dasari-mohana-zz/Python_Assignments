1cac46be0e636655bc34c219d3a1d8b9327c4b3d
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:58:57 +0530

    Add files via upload

---
+++ EquiTriangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:25:05 2020
+
+@author: MOHANA D
+"""
+
+#write a program to calculate area of an equilateral triangle
+import math
+x=float(input("enter side value:"))
+y=(math.sqrt(3)/4)*(x**2)
+print("area of equilateral triangle:",y)
