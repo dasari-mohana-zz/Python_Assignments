f35541b09f04a934d09e4a9eb65e8a3756bd3c60
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:00:39 +0530

    Add files via upload

---
+++ Km.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:32:59 2020
+
+@author: MOHANA D
+"""
+
+#wite a program to length in centimeter and convert into meter and kilometer
+cm=float(input("enter value:")) #cm is centimeter
+m=cm/100                        #m is meter
+km=cm/(1000*100)                #km is kilometer
+print(m)
+print(km)
