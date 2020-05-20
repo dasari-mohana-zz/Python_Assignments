586489ee56beba8b828e9796eec3dbc04ea5edc3
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:02:43 +0530

    Add files via upload

---
+++ RectglePerimeter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:36:36 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter length and breadth of rectangle and find its perimeter
+l=float(input("enter length value:"))#l=length
+b=float(input("enter breath value:"))#b=breadth
+p=(2*l)+(2*b) #p is perimeter
+print(p)
