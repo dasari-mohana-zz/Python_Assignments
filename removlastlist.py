a5051ac0a6b6c779882e471106937f11e17e0bd1
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:03:07 +0530

    Add files via upload

---
+++ removlastlist.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 17:07:26 2020
+
+@author: MOHANA D
+"""
+
+# Create a list and remove last element from it.
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
