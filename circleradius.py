46e00b444436d396d04a7b84d3b9e6f469df898e
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 19:57:36 +0530

    Add files via upload

---
+++ circleradius.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:34:54 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter radius of circle and find its diameter,circumference,and area
+r=float(input("enter radius of the circle:")) #r=radius
+d=2*r #d is diameter
+c=2*3.14*r #c is circumference
+a=3.14*(r**2) #a is area
+print("diameter=",d)
+print("circumference=",c)
+print("area=",a)
