f0365a2bf2ca0f3fbf07c3b34c4d006d0c5579c8
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-20 20:03:54 +0530

    Add files via upload

---
+++ squroot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:25:46 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter any number and calcute its square root
+x=float(input("enter number:"))
+y=x**0.5
+print("squre root of number is",y)
