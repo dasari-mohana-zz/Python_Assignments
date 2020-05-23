c5f9efef2b781b6601fd3454cc00dc5d14b3b5e8
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-23 15:38:44 +0530

    Add files via upload

---
+++ evnoddcond.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 15:32:38 2020
+
+@author: MOHANA D
+"""
+
+#write a program to check whether a number is even or odd using conditional operator
+x=int(input("enter number:"))
+if x%2==0:
+    print("{} is even number".format(x))
+else:
+    print("{} is odd number".format(x))
