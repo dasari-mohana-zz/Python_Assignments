c05718f56dc801bd696188be6ad7322f7387d305
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-23 16:00:18 +0530

    Add files via upload

---
+++ maxcond.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 15:53:25 2020
+
+@author: MOHANA D
+"""
+
+#write a program to find maximum between two numbers with/without using conditional operator.
+a=float(input("enter number a :"))
+b=float(input("enter number b :"))
+if a>b:
+    print("a={} is maximum nuber".format(a))
+else:
+    print("b={}is maximum number".format(b))
