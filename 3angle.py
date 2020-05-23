d82d2f6c1566cf5a98a40c4366d5b546f858a47d
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-23 16:33:50 +0530

    Add files via upload

---
+++ 3angle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 16:30:57 2020
+
+@author: MOHANA D
+"""
+
+#write a program to enter two angles of triangle and find third angle
+a=float(input("enter first angle value:"))
+b=float(input("enter second angle value:"))
+c=180-(a+b)
+print("thrid angle is:",c)
