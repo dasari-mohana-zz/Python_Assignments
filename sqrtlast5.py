3d27e30d2bd098b031ac8379a0a04838fa5b66f1
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-25 18:23:14 +0530

    Add files via upload

---
+++ sqrtlast5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 18:16:58 2020
+
+@author: MOHANA D
+"""
+
+
+#Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the last 5 elements in the list.
+def printvalues():
+	l1 = list()
+	for i in range(1,21):
+		l1.append(i**2)
+	print(l1[-5:])
+		
+printvalues()
