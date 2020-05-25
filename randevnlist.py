90f8bcd8e5245fed051d955dac9debe39a4dab92
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-25 17:41:20 +0530

    Add files via upload

---
+++ randevnlist.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 17:39:33 2020
+
+@author: MOHANA D
+"""
+
+
+#Please write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension
+import random
+
+num= random.randrange(0,11,2)   
+print(num)
