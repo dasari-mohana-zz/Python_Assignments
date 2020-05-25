bfcf6e94c7308eb5688bc3b2e79b76ddbb7f7e9c
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-25 18:13:59 +0530

    Add files via upload

---
+++ concatconsole.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 18:11:23 2020
+
+@author: MOHANA D
+"""
+#	Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+def printValue(s1,s2):
+	print (s1 + s2)
+
+printValue("a","b")
+
+#or
+
+s1 = input('Please enter the first string:\n')
+s2 = input('Please enter the second string:\n')
+
+print('Concatenated String =', s1 + s2)
