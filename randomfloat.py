8f5a99b19593b871c3dabf11415091d8aea00bc4
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-25 18:41:40 +0530

    Add files via upload

---
+++ randomfloat.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 18:38:33 2020
+
+@author: MOHANA D
+"""
+#Please generate a random float where the value is between 10 and 100 using Python math module.
+from random import uniform as randFloat
+print(randFloat(10,100))
+
+
+#or
+
+import random
+val= random.uniform(10,100)
+print(val)
