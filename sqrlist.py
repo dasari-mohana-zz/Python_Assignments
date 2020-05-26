ee7431b62727ff6d2c29d3f34beae06160a1aae7
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-26 17:15:09 +0530

    Add files via upload

---
+++ sqrlist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 17:07:16 2020
+
+@author: MOHANA D
+"""
+
+
+#Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included
+def printvalue():
+    l1=list()
+    for i in range(1,21):
+        l1.append(i*2)
+    print(l1)
+printvalue()
+
+
