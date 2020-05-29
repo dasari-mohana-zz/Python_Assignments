35c2395d0f134d6b84465df260408777924b2a8d
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-29 16:19:39 +0530

    Add files via upload

---
+++ tuplehalves.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 16:17:20 2020
+
+@author: MOHANA D
+"""
+#With a given tuple (1,2,3,4,5,6,7,8,9,10), 
+#write a program to print the first half values in one line and the last half values in one line.
+
+t1= (1,2,3,4,5,6,7,8,9,10)
+
+print(t1[:5])
+print(t1[5:])
