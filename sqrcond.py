88400adbc1c5ede57b6bff99357b0514dde9719e
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-29 19:25:16 +0530

    Add files via upload

---
+++ sqrcond.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 19:16:11 2020
+
+@author: MOHANA D
+"""
+
+#Write a method which can calculate square value of number
+num1=float(input("enter number:"))
+num2=num1*num1
+print("the square value of {0} is:{1}".format(num1,num2))
