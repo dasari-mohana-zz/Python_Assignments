32480169d45d385798c77c71ca5da13db3b976f1
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-30 22:38:01 +0530

    Add files via upload

---
+++ upperchar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 22:31:12 2020
+
+@author: MOHANA D
+"""
+
+#Write a program that accepts sequence of lines as input and prints the lines 
+#after making all characters in the sentence capitalized.
+
+cap=input("Enter the characters: ")
+cap.split(" ")
+print(cap.upper())
