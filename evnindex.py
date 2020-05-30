0daff51dc600b9d39d7d32bd09892a4f9c64e850
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-30 22:29:41 +0530

    Add files via upload

---
+++ evnindex.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 22:25:59 2020
+
+@author: MOHANA D
+"""
+
+#Please write a program which accepts a string from console and print the characters 
+#that have even indexes.
+
+a=input("enter string:")
+print(a[0::2])
