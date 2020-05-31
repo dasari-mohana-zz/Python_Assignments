eab1a587872b6cc0f5d0d957c57e4f3189317a81
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-31 19:03:43 +0530

    Add files via upload

---
+++ oddevn.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 18:54:14 2020
+
+@author: MOHANA D
+"""
+
+#Define a function that can accept an integer number as input and print the 
+#"It is an even number" if the number is even, otherwise print "It is an odd number".
+n=int(input("enter number: "))
+def func():
+    if n%2==0:
+        print("it is even number")
+    else:
+        print("it is odd number")
+func()
+    
---
+++ rand7_15.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 18:52:13 2020
+
+@author: MOHANA D
+"""
+
+#Please write a program to randomly print a integer number between 7 and 15 inclusive.
+import random
+x=random.randint(7,15)
+x
