4d9dec13e01b6dcc5554f285c80c668b36c8278a
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-05-31 17:18:49 +0530

    Add files via upload

---
+++ randomuniform.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 17:14:08 2020
+
+@author: MOHANA D
+"""
+
+# generate a random float where the value is between 5 and 95 using Python math module.
+
+import math
+import random
+val=random.uniform(5,95)  #uniform is used for float
+val
