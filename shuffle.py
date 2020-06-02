59bd06d5f3d4daa4564dec01e0ab07abb8ba337f
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-06-02 12:42:09 +0530

    Add files via upload

---
+++ shuffle.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 12:37:21 2020
+
+@author: MOHANA D
+"""
+
+#Please write a program to shuffle and print the list [3,6,7,8].
+from random import shuffle
+l1=[3,6,7,8]
+shuffle(l1)
+print(l1)
+
