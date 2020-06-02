6cf06099a90476f5affc23bb2a5782b5640b8f4b
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-06-02 14:43:39 +0530

    Add files via upload

---
+++ mapsqur.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 14:31:28 2020
+
+@author: MOHANA D
+"""
+
+#Write a program which can map() to make a list whose elements are square of elements in 
+
+
+l1=[1,2,3,4,5,6,7,8,9,10]
+even = list(map(lambda x: x**2, l1))
+even
