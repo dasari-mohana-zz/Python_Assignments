54508c5bf6891aea211a16a292627eccf7204cac
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-06-02 12:46:29 +0530

    Add files via upload

---
+++ rectanglearea.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 12:44:14 2020
+
+@author: MOHANA D
+"""
+
+#Define a class named Rectangle which can be constructed by a length and width. 
+#The Rectangle class has a method which can compute the area.
+l=float(input("enter length value:")) #l=length
+b=float(input("enter breath value:")) #b=breadth
+a=l*b #a is area
+print(a)
