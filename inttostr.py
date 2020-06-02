8a7d69664577e1ace8e99d81fc2444861e002374
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-06-02 15:35:31 +0530

    Add files via upload

---
+++ inttostr.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 15:28:48 2020
+
+@author: MOHANA D
+"""
+
+#Define a function that can convert a integer into a string and print it in console.
+x=1234
+y=str(x)
+y
+
